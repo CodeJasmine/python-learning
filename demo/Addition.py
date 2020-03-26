72510d1bedb463959f43b28476ab949cb72bba72
CodeJasmine <2816540069@qq.com>
2020-03-26 21:04:21 +0800

    demo练习-1次-修改

--- demo/Addition.py
+++ demo/Addition.py
@@ -1,15 +1,14 @@
 """
-加法测试，输入答案后会提示答案是否错误，如果错误，提示报错并返回正确答案
+加法测试，随机产生两个一位数的整数，然后使其相加，测试输入答案后是否正确，如果错误，提示报错并返回正确答案
 """
 
 import random
 
 a = random.randint(0, 9)
 b = random.randint(0, 9)
-c = a+b
 print(a, ' + ', b, ' = ?')
 value = int(input('请输入答案：'))
-if value == c:
+if value == (a+b):
     print('答案正确')
 else:
-    print('答案错误，正确答案为：', c)
+    print('答案错误，正确答案为：', (a+b))
